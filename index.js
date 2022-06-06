72b7f68565755bf688a9ebc5f36ec60bdd9efc56
urivat <rashad.porter18@gmail.com>
2022-06-05 20:10:54 -0400

    initial commit for blackjack game

---
+++ index.js
@@ -0,0 +1,16 @@
+let firstCard = 12
+let secondCard = 11
+let sum = firstCard + secondCard
+let hasBlackJack = false;
+let isAlive = true
+
+
+if(sum <= 20 ){
+    console.log("Do you want to draw a new card? 😊")
+} else if (sum === 21){
+    console.log(" Wohoo! You've got Blackjack! 🥳")
+    hasBlackJack = true;
+}else {
+    console.log("You're out of the game! 😭")
+    isAlive = false;
+}
