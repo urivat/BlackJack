3d19dc38490f86e6f7821fcb91d760243e33f97b
urivat <rashad.porter18@gmail.com>
2022-06-05 23:52:39 -0400

    added another button to log new cards as added.

--- index.js
+++ index.js
@@ -4,18 +4,26 @@ let sum = firstCard + secondCard
 let hasBlackJack = false;
 let isAlive = true
 let message = ''
+let messageEl = document.getElementById('message-el')
+let sumEl = document.getElementById('sum-el')
+let cardsEl = document.querySelector('#cards-el')
 
 function startGame(){
+    cardsEl.textContent = "Cards: " + firstCard + " " + secondCard;
+
     if(sum <= 20 ){
-    message = "Do you want to draw a new card? 😊"
+    message = "Do you want to draw a new card? "
 } else if (sum === 21){
-    message = "Wohoo! You've got Blackjack! 🥳"
+    message = "You've got Blackjack! "
     hasBlackJack = true;
 }else {
-    message = "You're out of the game! 😭"
+    message = "You're out of the game! "
     isAlive = false;
 }
-console.log(message)
+messageEl.textContent = message
+sumEl.textContent += sum 
 }
-
+function newCard() {
+    console.log('Drawing a new card from the deck!')
+}
 
